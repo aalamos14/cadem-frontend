9d361bfcd16ef2795fe34a0fd2e86e9f0e115d0d
ftocornal <ftocornal@uc.cl>
2024-01-16 17:24:23 -0300

    number

--- src/components/WaMeLink.jsx
+++ src/components/WaMeLink.jsx
@@ -7,7 +7,7 @@ const WaMeLink = () => {
             <a href="https://wa.me/56974786175" target="_blank" rel="noopener noreferrer">
                 <div className="flex items-center">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="WhatsApp" className="w-6 h-6 mr-2" />
-                    <span>Antonio Allende</span>
+                    <span>+56974786175</span>
                 </div>
                 
             </a>
