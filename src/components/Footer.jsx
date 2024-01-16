725fd0c00a91809609df587edde80f05183ec6a1
ftocornal <ftocornal@uc.cl>
2024-01-16 12:32:05 -0300

    email button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import WaMeLink from "./WaMeLink";
+import EmailLink from "./EmailLink";
 
 const Footer = () => {
 
     return (
-        <div className="flex justify-between bg-slate-500 px-5 py-3 sticky bottom-0">
-            <WaMeLink />
+        <div className="flex justify-between bg-slate-500 px-5 py-3 sticky bottom-0 text-white">
+            <div>
+                <WaMeLink />
+            </div>
+            <div>
+                <EmailLink />
+            </div>
         </div>
     );
 }
--- src/components/WaMeLink.jsx
+++ src/components/WaMeLink.jsx
@@ -4,9 +4,9 @@ const WaMeLink = () => {
 
     return (
         <div>
-            <a href="https://wa.me/56974786175" target="_blank" rel="noopener noreferrer" className="text-white">
+            <a href="https://wa.me/56974786175" target="_blank" rel="noopener noreferrer">
                 <div className="flex items-center">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="WhatsApp" className="w-7 h-7 mr-2" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="WhatsApp" className="w-6 h-6 mr-2" />
                     <span>Antonio Allende</span>
                 </div>
                 
