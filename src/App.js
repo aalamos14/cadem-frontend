dd9cbc367ea6ee50a86f9112ac3b164262b7a512
ftocornal <ftocornal@uc.cl>
2024-01-16 14:20:05 -0300

    aboslute

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Landing from './views/Landing';
-import Footer from './components/Footer';
-import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,35 +1,36 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 const Landing = () => {
     return (
         <div className="bg-[url('assets/images/image0.jpeg')] w-full h-screen bg-cover bg-center">
-            <div>
-                <div className="flex justify-center pt-10">
-                    <div className="flex flex-col items-center">
-                        <p className="text-5xl font-bold text-purple-700">Cadema</p>
-                    </div>
+            <div className="flex justify-center pt-10">
+                <div className="flex flex-col items-center">
+                    <p className="text-5xl font-bold text-purple-700">Cadema</p>
                 </div>
+            </div>
 
-                {/* flex with scroll x axis with 3 product boxes */}
-                <div className="flex items-center overflow-x-scroll scrollbar-hide mt-20">
-                    {/* product box with name and description */}
-                    <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
-                        <p className="text-xl font-bold text-slate-800 text-center">Carbón</p>
-                        <p className="text-md text-slate-700 text-center">Gran calidad en quebracho blanco y colorado con capacidad de producción para envasado personalizado a requerimiento del cliente.</p>
-                    </div>
-                    {/* product box with name and description */}
-                    <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
-                        <p className="text-xl font-bold text-slate-800 text-center">Miel</p>
-                        <p className="text-md text-slate-700 text-center">Actualmente manejamos miel de ulmo de la mas alta pureza.</p>
-                    </div>
-                    {/* product box with name and description */}
-                    <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
-                        {/* solucion de productos agricolas con acentos */}
-                        <p className="text-xl font-bold text-slate-800 text-center">Solución de productos agrícolas</p>
-                        <p className="text-md text-slate-700 text-center">Cuentanos un poco mas sobre tu proyecto para poder ofrecerte una solucion que se adapte a tus necesidades.</p>
-                    </div>
+            {/* flex with scroll x axis with 3 product boxes */}
+            <div className="flex items-center overflow-x-scroll scrollbar-hide mt-20">
+                {/* product box with name and description */}
+                <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
+                    <p className="text-xl font-bold text-slate-800 text-center">Carbón</p>
+                    <p className="text-md text-slate-700 text-center">Gran calidad en quebracho blanco y colorado con capacidad de producción para envasado personalizado a requerimiento del cliente.</p>
                 </div>
-
+                {/* product box with name and description */}
+                <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
+                    <p className="text-xl font-bold text-slate-800 text-center">Miel</p>
+                    <p className="text-md text-slate-700 text-center">Actualmente manejamos miel de ulmo de la mas alta pureza.</p>
+                </div>
+                {/* product box with name and description */}
+                <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
+                    {/* solucion de productos agricolas con acentos */}
+                    <p className="text-xl font-bold text-slate-800 text-center">Solución de productos agrícolas</p>
+                    <p className="text-md text-slate-700 text-center">Cuentanos un poco mas sobre tu proyecto para poder ofrecerte una solucion que se adapte a tus necesidades.</p>
+                </div>
+            </div>
+            <div className="absolute bottom-0 w-full">
+                <Footer />
             </div>
         </div>
     );
