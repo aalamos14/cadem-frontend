3e86322ed957f174856da9638c0abbdec9192053
ftocornal <ftocornal@uc.cl>
2024-01-16 12:08:12 -0300

    whatsapp adjust

--- src/components/WaMeLink.jsx
+++ src/components/WaMeLink.jsx
@@ -6,7 +6,7 @@ const WaMeLink = () => {
         <div>
             <a href="https://wa.me/56974786175" target="_blank" rel="noopener noreferrer" className="text-white">
                 <div className="flex items-center">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="WhatsApp" className="w-5 h-5 mr-2" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="WhatsApp" className="w-7 h-7 mr-2" />
                     <span>Antonio Allende</span>
                 </div>
                 
