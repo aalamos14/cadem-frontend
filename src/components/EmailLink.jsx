520fade949810f0e6281963d620a03361424099c
ftocornal <ftocornal@uc.cl>
2024-01-16 17:34:09 -0300

    fix

--- src/components/EmailLink.jsx
+++ src/components/EmailLink.jsx
@@ -8,8 +8,7 @@ const EmailLink = () => {
                 <div className="flex items-center">
                     <img src="https://quotall.com/wp-content/uploads/2020/04/Support-Email-Icon-White.png" alt="Email" className="w-7 h-7 mr-2" />
                     <button onClick={() => window.location = 'mailto:aalamos14@gmail.com'}>aalamos14@gmail.com</button>
-                </div>
-                
+                </div>  
             </a>
         </div>
     );
