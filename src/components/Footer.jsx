ca9c636eb4027438c443376824a76836e424f2aa
ftocornal <ftocornal@uc.cl>
2024-01-16 12:05:21 -0300

    whatsapp button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import WaMeLink from "./WaMeLink";
 
 const Footer = () => {
 
     return (
         <div className="flex justify-between bg-slate-500 px-5 py-3 sticky bottom-0">
-            <div className="text-white text-left">
-                <span className="font-bold text-3xl tracking-tight">Numero: +56993430490</span>
-            </div>
+            <WaMeLink />
         </div>
     );
 }
