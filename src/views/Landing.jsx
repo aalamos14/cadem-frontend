8a66328f6416cb774c3504665caea517a88f36a6
ftocornal <ftocornal@uc.cl>
2024-01-16 13:18:59 -0300

    fix center

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -15,18 +15,18 @@ const Landing = () => {
                 <div className="flex items-center overflow-x-scroll scrollbar-hide mt-20">
                     {/* product box with name and description */}
                     <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
-                        <p className="text-xl font-bold text-slate-800">Carbón</p>
+                        <p className="text-xl font-bold text-slate-800 text-center">Carbón</p>
                         <p className="text-md text-slate-700 text-center">Gran calidad en quebracho blanco y colorado con capacidad de producción para envasado personalizado a requerimiento del cliente.</p>
                     </div>
                     {/* product box with name and description */}
                     <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
-                        <p className="text-xl font-bold text-slate-800">Miel</p>
+                        <p className="text-xl font-bold text-slate-800 text-center">Miel</p>
                         <p className="text-md text-slate-700 text-center">Actualmente manejamos miel de ulmo de la mas alta pureza.</p>
                     </div>
                     {/* product box with name and description */}
                     <div className="flex flex-col items-center bg-white rounded-lg p-5 m-2 min-w-[250px]">
                         {/* solucion de productos agricolas con acentos */}
-                        <p className="text-xl font-bold text-slate-800">Solución de productos agrícolas</p>
+                        <p className="text-xl font-bold text-slate-800 text-center">Solución de productos agrícolas</p>
                         <p className="text-md text-slate-700 text-center">Cuentanos un poco mas sobre tu proyecto para poder ofrecerte una solucion que se adapte a tus necesidades.</p>
                     </div>
                 </div>
