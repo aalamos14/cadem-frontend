de484bdd23ef76eb819861484f5b32ad711429c7
ftocornal <ftocornal@uc.cl>
2024-01-16 17:27:22 -0300

    fix 2

--- src/components/EmailLink.jsx
+++ src/components/EmailLink.jsx
@@ -6,8 +6,8 @@ const EmailLink = () => {
         <div>
             <a href="" target="_blank" rel="noopener noreferrer">
                 <div className="flex items-center">
-                <img src="https://quotall.com/wp-content/uploads/2020/04/Support-Email-Icon-White.png" alt="Email" className="w-7 h-7 mr-2" />
-                <button onClick={() => window.location = 'mailto:aalamos14@gmail.com'}>aalamos14@gmail.com</button>
+                    <img src="https://quotall.com/wp-content/uploads/2020/04/Support-Email-Icon-White.png" alt="Email" className="w-7 h-7 mr-2" />
+                    <button onClick={() => window.location = 'mailto:aalamos14@gmail.com'}>aalamos14@gmail.com</button>
                 </div>
                 
             </a>
